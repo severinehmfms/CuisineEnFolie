31f3551d9c369b457db9058cb7f2976f6cc23198
Séverine HM <severine.hori.maitrehut@fms-ea.com>
2026-09-10 14:59:39 +0200

    exemple site pour thread unique

---
+++ exemple_site.py
@@ -0,0 +1,35 @@
+import logging
+import threading
+import time
+
+def thread_function(name):
+    logging.info("Thread %s: starting", name)
+    time.sleep(2)
+    logging.info("Thread %s: finishing", name)
+
+def ma_function(name):
+    logging.info("Yes vous avez démarré le thread! %s: starting", name)
+    time.sleep(2)
+    logging.info("Le thread est fini ! %s: finishing", name)
+
+if __name__ == "__main__":
+    format = "%(asctime)s: %(message)s"
+    logging.basicConfig(format=format, level=logging.INFO,
+                        datefmt="%H:%M:%S")
+
+    logging.info("Main    : before creating thread")
+
+    # Par défaut un thread n'est pas un daemon, donc il peut se terminer.
+    #x = threading.Thread(target=ma_function, args=(1,))
+
+    # Le thread est défini comme étant un daemon c'est à dire qu'il est tué quand le programme est fini. Du coup mon thread a pas pu se terminer !
+    x = threading.Thread(target=ma_function, args=(1,), daemon=True)
+    logging.info("Main    : before running thread")
+    x.start()
+    logging.info("Main    : wait for the thread to finish")
+
+    #Pour dire à un thread d'attendre qu'un autre thread finisse, tu appelles .join(). Si tu décommentes cette ligne, le thread principal va se mettre en pause et attendre que le thread x ait fini de s'exécuter.
+    x.join()
+
+
+    logging.info("Main    : all done")
